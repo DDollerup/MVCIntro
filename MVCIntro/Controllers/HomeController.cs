bb7ee6477721dbcb26e9d7fe12ab481253010a2f
PDDaniel <daniel@proper-dog.com>
2016-03-07 10:20:37 +0100

    Initial Commit

---
+++ MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCIntro.Models.BaseModels;
+
+namespace MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Home home = new Home();
+            home.HomeID = 1;
+            home.Text = "Welcome to my first MVC application!";
+            home.Image = "Cookie.png";
+
+            return View(home);
+        }
+
+        public ActionResult About()
+        {
+            About about = new About();
+            about.AboutID = 1;
+            about.Text = "I'm a guy, that makes MVC Applications";
+            about.Image = "N/A";
+
+            return View(about);
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCIntro/Models/BaseModels/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCIntro.Models.BaseModels
+{
+    public class Home
+    {
+        public int HomeID { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+    }
+}
